075cf8a7db1ddbbc8387402268b8a80328da2159
Rob Baile <robbaile12@outlook.com>
2019-10-08 16:36:29 +0100

    completed chapter_4

--- exercise_4.rb
+++ exercise_4.rb
@@ -6,11 +6,24 @@
 # other formats
 
 def pig_latin(word)
-
+  vowels = ["a", "e", "i", "o", "u"]
+  index_to_split = []
+  new_word = ""
+  if word[0..1].downcase === "qu"
+    new_word = word[2..-1] + word[0..1] + "ay"
+  elsif !vowels.include?(word.chars.first)
+    word.split("").each do |letter|
+      index_to_split << word.index(letter) if vowels.include?(letter)
+    end
+    new_word = word[index_to_split[0]..-1] + word[0...index_to_split[0]] + "ay"
+  else
+    new_word = word + "way"
+  end
+  word === word.capitalize ? new_word.capitalize : new_word
 end
 
 ## Tests:
 
-require_relative './helpers/assert_equal'
-assert_equal(pig_latin('Dog'), 'Ogday')
-assert_equal(pig_latin('cram'), 'amcray')
+require_relative "./helpers/assert_equal"
+assert_equal(pig_latin("Dog"), "Ogday")
+assert_equal(pig_latin("cram"), "amcray")
