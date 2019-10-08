8140858c2cd2d7bc0775eeac951dddbd9cda5a10
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-08 14:56:52 +0100

    started exercise2

--- exercise_2.rb
+++ exercise_2.rb
@@ -8,13 +8,20 @@
 # Still assume the words are lowercase.
 
 def pig_latin(word)
-
+  vowels = ["a", "e", "i", "o", "u"]
+  if !vowels.include?(word.chars.first)
+    consonants = word.split("").each { |letter| }
+    consonants
+    #word[1..-1] + word.chars.first + "ay"
+  else
+    word + "way"
+  end
 end
 
 ## Tests:
 
-require_relative './helpers/assert_equal'
-assert_equal(pig_latin('cram'), 'amcray')
-assert_equal(pig_latin('sprinkle'), 'inklespray')
-assert_equal(pig_latin('dog'), 'ogday')
-assert_equal(pig_latin('up'), 'upway')
+require_relative "./helpers/assert_equal"
+assert_equal(pig_latin("cram"), "amcray")
+assert_equal(pig_latin("sprinkle"), "inklespray")
+assert_equal(pig_latin("dog"), "ogday")
+assert_equal(pig_latin("up"), "upway")
